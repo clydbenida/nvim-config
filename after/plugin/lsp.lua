15a71da73080a332c6cf8efe75b76b1675c3fb14
bbenida <bbenida@penbrothers.com>
2024-04-09 22:39:04 +0800

    More config changes - Added recommended preset in lsp.lua - Added more mappings from theprimeagen tutorial in lsp.lua - edited the tabwidth from 4 to 2

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,10 +1,6 @@
 local lsp_zero = require('lsp-zero')
 
-lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
-end)
+lsp_zero.preset("recommended")
 
 -- here you can setup the language servers
 require('mason').setup({})
@@ -19,8 +15,16 @@ require('mason-lspconfig').setup({
   },
 })
 
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp_zero.defaults.cmp_mappings({
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ['<C-space>'] = cmp.mapping.complete(),
+})
+
 lsp_zero.on_attach(function(client, bufnr)
-    print("help")
     local opts = {buffer = bufnr, remap = false}
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
 end)
