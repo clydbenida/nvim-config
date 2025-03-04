ac62692926fe216f9749ba5aedbf424f3b588567
bbenida <clyd.benidz@gmail.com>
2025-03-04 14:12:41 +0800

    idk just pushing staged changes lmao

--- after/plugin/formatting.lua
+++ after/plugin/formatting.lua
@@ -12,8 +12,8 @@ conform.setup({
     python = { "isort", "black" },
     -- Use a sub-list to run only the first available formatter
     javascript = { { "prettierd", "prettier" } },
-    typescript = { "prettier" },
-    typescriptreact = { "prettier" },
+    typescript = { { "prettier", "prettierd" } },
+    typescriptreact = { { "prettier", "prettierd" } },
   },
 
   -- format_on_save = {
@@ -21,41 +21,41 @@ conform.setup({
   --   lsp_fallback = true,
   -- },
 
-  format_on_save = {
-    lsp_fallback = true,
-    timeout_ms = 500,
-    callback = function(bufnr)
-      local ignore_filetypes = { "lua" }
-      if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
-        return { timeout_ms = 500, lsp_fallback = true }
-      end
-      local lines =
-          vim.fn.system("git diff --unified=0 " .. vim.fn.bufname(bufnr)):gmatch("[^\n\r]+")
-      local ranges = {}
-      for line in lines do
-        if line:find("^@@") then
-          local line_nums = line:match("%+.- ")
-          if line_nums:find(",") then
-            local _, _, first, second = line_nums:find("(%d+),(%d+)")
-            table.insert(ranges, {
-              start = { tonumber(first), 0 },
-              ["end"] = { tonumber(first) + tonumber(second), 0 },
-            })
-          else
-            local first = tonumber(line_nums:match("%d+"))
-            table.insert(ranges, {
-              start = { first, 0 },
-              ["end"] = { first + 1, 0 },
-            })
-          end
-        end
-      end
-      local format = require("conform").format
-      for _, range in pairs(ranges) do
-        format({ range = range })
-      end
-    end,
-  }
+  -- format_on_save = {
+  --   lsp_fallback = true,
+  --   timeout_ms = 500,
+  --   callback = function(bufnr)
+  --     local ignore_filetypes = { "lua" }
+  --     if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+  --       return { timeout_ms = 500, lsp_fallback = true }
+  --     end
+  --     local lines =
+  --         vim.fn.system("git diff --unified=0 " .. vim.fn.bufname(bufnr)):gmatch("[^\n\r]+")
+  --     local ranges = {}
+  --     for line in lines do
+  --       if line:find("^@@") then
+  --         local line_nums = line:match("%+.- ")
+  --         if line_nums:find(",") then
+  --           local _, _, first, second = line_nums:find("(%d+),(%d+)")
+  --           table.insert(ranges, {
+  --             start = { tonumber(first), 0 },
+  --             ["end"] = { tonumber(first) + tonumber(second), 0 },
+  --           })
+  --         else
+  --           local first = tonumber(line_nums:match("%d+"))
+  --           table.insert(ranges, {
+  --             start = { first, 0 },
+  --             ["end"] = { first + 1, 0 },
+  --           })
+  --         end
+  --       end
+  --     end
+  --     local format = require("conform").format
+  --     for _, range in pairs(ranges) do
+  --       format({ range = range })
+  --     end
+  --   end,
+  -- }
 })
 
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
